9772c49fb92f8223cf7b1b90fad60004764377ed
Skelmis <47520067+Skelmis@users.noreply.github.com>
2020-08-08 02:54:30 +1200

    Add files via upload

--- cogs/moderation.py
+++ cogs/moderation.py
@@ -1,11 +1,15 @@
 import re
+import datetime
+from copy import deepcopy
 
 import asyncio
 import discord
-from discord.ext import commands
+from discord.ext import commands, tasks
+from dateutil.relativedelta import relativedelta
 
 time_regex = re.compile("(?:(\d{1,5})(h|s|m|d))+?")
-time_dict = {'h': 3600, 's': 1, 'm': 60, 'd': 86400}
+time_dict = {"h": 3600, "s": 1, "m": 60, "d": 86400}
+
 
 class TimeConverter(commands.Converter):
     async def convert(self, ctx, argument):
@@ -16,82 +20,50 @@ class TimeConverter(commands.Converter):
             try:
                 time += time_dict[value] * float(key)
             except KeyError:
-                raise commands.BadArgument(f"{value} is an invalid time key! h|m|s|d are valid arguments")
+                raise commands.BadArgument(
+                    f"{value} is an invalid time key! h|m|s|d are valid arguments"
+                )
             except ValueError:
                 raise commands.BadArgument(f"{key} is not a number!")
-        return time
+        return round(time)
 
 
 class Moderation(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
+        self.mute_task = self.check_current_mutes.start()
 
-    @commands.Cog.listener()
-    async def on_ready(self):
-        print(f"{self.__class__.__name__} Cog has been loaded\n-----")
+    def cog_unload(self):
+        self.mute_task.cancel()
 
-    @commands.command(
-        name="kick",
-        description="A command which kicks a given user",
-        usage="<user> [reason]",
-    )
-    @commands.guild_only()
-    @commands.has_guild_permissions(ban_members=True)
-    async def kick(self, ctx, member: discord.Member, *, reason=None):
-        await ctx.guild.kick(user=member, reason=reason)
+    @tasks.loop(minutes=5)
+    async def check_current_mutes(self):
+        currentTime = datetime.datetime.now()
+        mutes = deepcopy(self.bot.muted_users)
+        for key, value in mutes.items():
+            if value['muteDuration'] is None:
+                continue
 
-        # Using our past episodes knowledge can we make the log channel dynamic?
-        embed = discord.Embed(
-            title=f"{ctx.author.name} kicked: {member.name}", description=reason
-        )
-        await ctx.send(embed=embed)
+            unmuteTime = value['mutedAt'] + relativedelta(seconds=value['muteDuration'])
 
-    @commands.command(
-        name="ban",
-        description="A command which bans a given user",
-        usage="<user> [reason]",
-    )
-    @commands.guild_only()
-    @commands.has_guild_permissions(ban_members=True)
-    async def ban(self, ctx, member: discord.Member, *, reason=None):
-        await ctx.guild.ban(user=member, reason=reason)
+            if currentTime >= unmuteTime:
+                guild = self.bot.get_guild(value['guildId'])
+                member = guild.get_member(value['_id'])
 
-        # Using our past episodes knowledge can we make the log channel dynamic?
-        embed = discord.Embed(
-            title=f"{ctx.author.name} banned: {member.name}", description=reason
-        )
-        await ctx.send(embed=embed)
+                role = discord.utils.get(guild.roles, name="Muted")
+                if role in member.roles:
+                    await member.remove_roles(role)
+                    print(f"Unmuted: {member.display_name}")
 
-    @commands.command(
-        name="unban",
-        description="A command which unbans a given user",
-        usage="<user> [reason]",
-    )
-    @commands.guild_only()
-    @commands.has_guild_permissions(ban_members=True)
-    async def unban(self, ctx, member, *, reason=None):
-        member = await self.bot.fetch_user(int(member))
-        await ctx.guild.unban(member, reason=reason)
+                await self.bot.mutes.delete(member.id)
+                try:
+                    self.bot.muted_users.pop(member.id)
+                except KeyError:
+                    pass
 
-        embed = discord.Embed(
-            title=f"{ctx.author.name} unbanned: {member.name}", description=reason
-        )
-        await ctx.send(embed=embed)
-
-    @commands.command(
-        name="purge",
-        description="A command which purges the channel it is called in",
-        usage="[amount]",
-    )
-    @commands.guild_only()
-    @commands.is_owner()
-    async def purge(self, ctx, amount=15):
-        await ctx.channel.purge(limit=amount + 1)
-        embed = discord.Embed(
-            title=f"{ctx.author.name} purged: {ctx.channel.name}",
-            description=f"{amount} messages were cleared",
-        )
-        await ctx.send(embed=embed, delete_after=15)
+    @check_current_mutes.before_loop
+    async def before_check_current_mutes(self):
+        await self.bot.wait_until_ready()
 
     @commands.command(
         name='mute',
@@ -105,15 +77,53 @@ class Moderation(commands.Cog):
             await ctx.send("No muted role was found! Please create one called `Muted`")
             return
 
-        await member.add_roles(role)
-        await ctx.send((f"Muted `{member.display_name}` for {time}s." if time else f"Muted `{member.display_name}`."))
+        try:
+            if self.bot.muted_users[member.id]:
+                await ctx.send("This user is already muted")
+                return
+        except KeyError:
+            pass
 
-        if time:
+        data = {
+            '_id': member.id,
+            'mutedAt': datetime.datetime.now(),
+            'muteDuration': time or None,
+            'mutedBy': ctx.author.id,
+            'guildId': ctx.guild.id,
+        }
+        await self.bot.mutes.upsert(data)
+        self.bot.muted_users[member.id] = data
+
+        await member.add_roles(role)
+
+        if not time:
+            await ctx.send(f"Muted {member.display_name}")
+        else:
+            minutes, seconds = divmod(time, 60)
+            hours, minutes = divmod(minutes, 60)
+            if int(hours):
+                await ctx.send(
+                    f"Muted {member.display_name} for {hours} hours, {minutes} minutes and {seconds} seconds"
+                )
+            elif int(minutes):
+                await ctx.send(
+                    f"Muted {member.display_name} for {minutes} minutes and {seconds} seconds"
+                )
+            elif int(seconds):
+                await ctx.send(f"Muted {member.display_name} for {seconds} seconds")
+
+        if time and time < 300:
             await asyncio.sleep(time)
 
             if role in member.roles:
                 await member.remove_roles(role)
                 await ctx.send(f"Unmuted `{member.display_name}`")
+
+            await self.bot.mutes.delete(member.id)
+            try:
+                self.bot.muted_users.pop(member.id)
+            except KeyError:
+                pass
 
     @commands.command(
         name='unmute',
@@ -127,11 +137,19 @@ class Moderation(commands.Cog):
             await ctx.send("No muted role was found! Please create one called `Muted`")
             return
 
+        await self.bot.mutes.delete(member.id)
+        try:
+            self.bot.muted_users.pop(member.id)
+        except KeyError:
+            pass
+
         if role not in member.roles:
             await ctx.send("This member is not muted.")
+            return
 
         await member.remove_roles(role)
         await ctx.send(f"Unmuted `{member.display_name}`")
+
 
 def setup(bot):
     bot.add_cog(Moderation(bot))
