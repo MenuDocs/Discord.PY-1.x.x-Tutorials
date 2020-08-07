a9ff241ab0be61c55966e1a2768ca0505734875b
Skelmis <47520067+Skelmis@users.noreply.github.com>
2020-08-08 02:55:41 +1200

    Lol oops, here it is

--- cogs/moderation.py
+++ cogs/moderation.py
@@ -70,7 +70,7 @@ class Moderation(commands.Cog):
         description="Mutes a given user for x time!",
         ussage='<user> [time]'
     )
-    #@commands.has_permissions(manage_roles=True)
+    @commands.has_permissions(manage_roles=True)
     async def mute(self, ctx, member: discord.Member, *, time: TimeConverter=None):
         role = discord.utils.get(ctx.guild.roles, name="Muted")
         if not role:
@@ -130,7 +130,7 @@ class Moderation(commands.Cog):
         description="Unmuted a member!",
         usage='<user>'
     )
-    #@commands.has_permissions(manage_roles=True)
+    @commands.has_permissions(manage_roles=True)
     async def unmute(self, ctx, member: discord.Member):
         role = discord.utils.get(ctx.guild.roles, name="Muted")
         if not role:
@@ -149,6 +149,69 @@ class Moderation(commands.Cog):
 
         await member.remove_roles(role)
         await ctx.send(f"Unmuted `{member.display_name}`")
+
+    @commands.command(
+        name="kick",
+        description="A command which kicks a given user",
+        usage="<user> [reason]",
+    )
+    @commands.guild_only()
+    @commands.has_guild_permissions(ban_members=True)
+    async def kick(self, ctx, member: discord.Member, *, reason=None):
+        await ctx.guild.kick(user=member, reason=reason)
+
+        # Using our past episodes knowledge can we make the log channel dynamic?
+        embed = discord.Embed(
+            title=f"{ctx.author.name} kicked: {member.name}", description=reason
+        )
+        await ctx.send(embed=embed)
+
+    @commands.command(
+        name="ban",
+        description="A command which bans a given user",
+        usage="<user> [reason]",
+    )
+    @commands.guild_only()
+    @commands.has_guild_permissions(ban_members=True)
+    async def ban(self, ctx, member: discord.Member, *, reason=None):
+        await ctx.guild.ban(user=member, reason=reason)
+
+        # Using our past episodes knowledge can we make the log channel dynamic?
+        embed = discord.Embed(
+            title=f"{ctx.author.name} banned: {member.name}", description=reason
+        )
+        await ctx.send(embed=embed)
+
+    @commands.command(
+        name="unban",
+        description="A command which unbans a given user",
+        usage="<user> [reason]",
+    )
+    @commands.guild_only()
+    @commands.has_guild_permissions(ban_members=True)
+    async def unban(self, ctx, member, *, reason=None):
+        member = await self.bot.fetch_user(int(member))
+        await ctx.guild.unban(member, reason=reason)
+
+        embed = discord.Embed(
+            title=f"{ctx.author.name} unbanned: {member.name}", description=reason
+        )
+        await ctx.send(embed=embed)
+
+    @commands.command(
+        name="purge",
+        description="A command which purges the channel it is called in",
+        usage="[amount]",
+    )
+    @commands.guild_only()
+    @commands.is_owner()
+    async def purge(self, ctx, amount=15):
+        await ctx.channel.purge(limit=amount + 1)
+        embed = discord.Embed(
+            title=f"{ctx.author.name} purged: {ctx.channel.name}",
+            description=f"{amount} messages were cleared",
+        )
+        await ctx.send(embed=embed, delete_after=15)
 
 
 def setup(bot):
