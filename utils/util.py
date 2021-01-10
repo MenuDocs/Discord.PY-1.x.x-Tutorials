96de52e66bd6b4cfc092b35e39167fe30b7b619a
Skelmis <47520067+Skelmis@users.noreply.github.com>
2021-01-10 20:54:36 +1300

    Add files via upload

--- utils/util.py
+++ utils/util.py
@@ -45,3 +45,10 @@ async def GetMessage(
             return msg.content
     except asyncio.TimeoutError:
         return False
+
+
+def clean_code(content):
+    if content.startswith("```") and content.endswith("```"):
+        return "\n".join(content.split("\n")[1:])[:-3]
+    else:
+        return content
