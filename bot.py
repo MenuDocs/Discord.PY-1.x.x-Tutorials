b50532fbc742fd62ce3d0fd6f14f5bbd89bcda2b
Skelmis <47520067+Skelmis@users.noreply.github.com>
2021-01-10 20:54:20 +1300

    Add files via upload

--- bot.py
+++ bot.py
@@ -1,8 +1,13 @@
 # Standard libraries
+import contextlib
+import io
 import os
 import logging
 
 # Third party libraries
+import textwrap
+from traceback import format_exception
+
 import discord
 from pathlib import Path
 import motor.motor_asyncio
@@ -11,6 +16,7 @@ from discord.ext import commands
 # Local code
 import utils.json_loader
 from utils.mongo import Document
+from utils.util import clean_code, Pag
 
 cwd = Path(__file__).parents[0]
 cwd = str(cwd)
@@ -89,7 +95,7 @@ async def on_ready():
         f"-----\nLogged in as: {bot.user.name} : {bot.user.id}\n-----\nMy current prefix is: {bot.DEFAULTPREFIX}\n-----"
     )
     await bot.change_presence(
-        activity=discord.Game(name="Waves in brail")
+        activity=discord.Game(name="Cries in Binary | 00111010 00101000")
     )  # This changes the bots 'activity'
 
     for document in await bot.config.get_all():
@@ -129,6 +135,46 @@ async def on_message(message):
     await bot.process_commands(message)
 
 
+@bot.command(name="eval", aliases=["exec"])
+@commands.is_owner()
+async def _eval(ctx, *, code):
+    code = clean_code(code)
+
+    local_variables = {
+        "discord": discord,
+        "commands": commands,
+        "bot": bot,
+        "ctx": ctx,
+        "channel": ctx.channel,
+        "author": ctx.author,
+        "guild": ctx.guild,
+        "message": ctx.message
+    }
+
+    stdout = io.StringIO()
+
+    try:
+        with contextlib.redirect_stdout(stdout):
+            exec(
+                f"async def func():\n{textwrap.indent(code, '    ')}", local_variables,
+            )
+
+            obj = await local_variables["func"]()
+            result = f"{stdout.getvalue()}\n-- {obj}\n"
+    except Exception as e:
+        result = "".join(format_exception(e, e, e.__traceback__))
+
+    pager = Pag(
+        timeout=100,
+        entries=[result[i: i + 2000] for i in range(0, len(result), 2000)],
+        length=1,
+        prefix="```py\n",
+        suffix="```"
+    )
+
+    await pager.start(ctx)
+
+
 if __name__ == "__main__":
     # When running this file, if it is the 'main' file
     # I.E its not being imported from another python file run this
@@ -138,6 +184,7 @@ if __name__ == "__main__":
     bot.mutes = Document(bot.db, "mutes")
     bot.invites = Document(bot.db, "invites")
     bot.command_usage = Document(bot.db, "command_usage")
+    bot.reaction_roles = Document(bot.db, "reaction_roles")
 
     for file in os.listdir(cwd + "/cogs"):
         if file.endswith(".py") and not file.startswith("_"):
