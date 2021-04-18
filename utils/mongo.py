51dfd6b934f560dd21563ee32c12c48af5f275ee
Skelmis <47520067+Skelmis@users.noreply.github.com>
2021-04-19 10:18:26 +1200

    Update a severely outdated file

--- utils/mongo.py
+++ utils/mongo.py
@@ -1,4 +1,3 @@
-import logging
 import collections
 
 """
@@ -11,215 +10,124 @@ on init and the document to create an instance on and boom
 
 class Document:
     def __init__(self, connection, document_name):
-        """
-        Our init function, sets up the conenction to the specified document
-        Params:
-         - connection (Mongo Connection) : Our database connection
-         - documentName (str) : The document this instance should be
-        """
         self.db = connection[document_name]
-        self.logger = logging.getLogger(__name__)
 
     # <-- Pointer Methods -->
-    async def update(self, dict, *args, **kwargs):
-        """
-        For simpler calls, points to self.update_by_id
-        """
-        await self.update_by_id(dict, *args, **kwargs)
+    async def get_by_id(self, data_id):
+        return await self.find_by_id(data_id)
 
-    async def get_all(self, filter={}, *args, **kwargs):
-        """
-        Returns a list of all data in the document
-        """
-        return await self.db.find(filter, *args, **kwargs).to_list(None)
+    async def find(self, data_id):
+        return await self.find_by_id(data_id)
 
-    async def get_by_id(self, id):
-        """
-        This is essentially find_by_id so point to that
-        """
-        return await self.find_by_id(id)
-
-    async def find(self, id):
-        """
-        For simpler calls, points to self.find_by_id
-        """
-        return await self.find_by_id(id)
-
-    async def delete(self, id):
-        """
-        For simpler calls, points to self.delete_by_id
-        """
-        await self.delete_by_id(id)
+    async def delete(self, data_id):
+        await self.delete_by_id(data_id)
 
     # <-- Actual Methods -->
-    async def find_by_id(self, id):
-        """
-        Returns the data found under `id`
-        Params:
-         -  id () : The id to search for
-        Returns:
-         - None if nothing is found
-         - If somethings found, return that
-        """
-        return await self.db.find_one({"_id": id})
+    async def update(self, data, *args, **kwargs):
+        await self.update_by_id(data, *args, **kwargs)
 
-    async def find_by_custom(self, filter):
-        """
-        Returns data found with the custom filter rather then just id
-        Params:
-         - filter (dict) : The filter to search by
-        """
-        if not isinstance(filter, collections.abc.Mapping):
-            raise TypeError("Expected Dictionary.")
+    async def get_all(self, filter_dict=None, *args, **kwargs):
+        if filter_dict is None:
+            filter_dict = {}
 
-        return await self.db.find_one(filter)
+        return await self.db.find(filter_dict, *args, **kwargs).to_list(None)
 
-    async def find_many_by_custom(self, filter):
-        """Find all matching filter"""
-        if not isinstance(filter, collections.abc.Mapping):
-            raise TypeError("Expected Dictionary.")
+    async def find_by_id(self, data_id):
+        return await self.db.find_one({"_id": data_id})
 
-        return await self.db.find(filter).to_list(None)
+    async def find_by_custom(self, filter_dict):
+        self.__ensure_dict(filter_dict)
 
-    async def delete_by_id(self, id):
-        """
-        Deletes all items found with _id: `id`
-        Params:
-         -  id () : The id to search for and delete
-        """
-        # Raise if the _id does not exist in database
-        if await self.find_by_id(id) is None:
+        return await self.db.find_one(filter_dict)
+
+    async def find_many_by_custom(self, filter_dict):
+        self.__ensure_dict(filter_dict)
+
+        return await self.db.find(filter_dict).to_list(None)
+
+    async def delete_by_id(self, data_id):
+        if await self.find_by_id(data_id) is None:
             return
 
-        await self.db.delete_many({"_id": id})
+        await self.db.delete_many({"_id": data_id})
 
-    async def delete_by_custom(self, filter):
-        """
-        Delete all items matching the 'filter'
-        """
-        if not isinstance(filter, collections.abc.Mapping):
-            raise TypeError("Expected Dictionary.")
+    async def delete_by_custom(self, filter_dict):
+        self.__ensure_dict(filter_dict)
 
-        if await self.find_by_custom(filter) is None:
+        if await self.find_by_custom(filter_dict) is None:
             return
 
         # TODO Discuss this in ep 27.3
-        return await self.db.delete_many(filter)
+        return await self.db.delete_many(filter_dict)
 
-    async def insert(self, dict):
-        """
-        insert something into the db
-        Params:
-        - dict (Dictionary) : The Dictionary to insert
-        """
-        # Check if its actually a Dictionary
-        if not isinstance(dict, collections.abc.Mapping):
-            raise TypeError("Expected Dictionary.")
+    async def insert(self, data):
+        self.__ensure_dict(data)
 
-        await self.db.insert_one(dict)
+        await self.db.insert_one(data)
 
-    async def upsert(self, dict, option="set", *args, **kwargs):
-        """
-        Makes a new item in the document, if it already exists
-        it will update that item instead
-        This function parses an input Dictionary to get
-        the relevant information needed to insert.
-        Supports inserting when the document already exists
-        Params:
-         - dict (Dictionary) : The dict to insert
-        """
-        await self.update_by_id(dict, option, upsert=True, *args, **kwargs)
+    async def upsert(self, data, option="set", *args, **kwargs):
+        await self.update_by_id(data, option, upsert=True, *args, **kwargs)
 
-    async def update_by_id(self, dict, option="set", *args, **kwargs):
-        """
-        For when a document already exists in the data
-        and you want to update something in it
-        This function parses an input Dictionary to get
-        the relevant information needed to update.
-        Params:
-         - dict (Dictionary) : The dict to insert
-        """
-        # Check if its actually a Dictionary
-        if not isinstance(dict, collections.abc.Mapping):
-            raise TypeError("Expected Dictionary.")
+    async def update_by_id(self, data, option="set", *args, **kwargs):
+        self.__ensure_dict(data)
+        self.__ensure_id(data)
 
-        # Always use your own _id
-        if not dict["_id"]:
-            raise KeyError("_id not found in supplied dict.")
+        if await self.find_by_id(data["_id"]) is None:
+            return await self.insert(data)
 
-        if await self.find_by_id(dict["_id"]) is None:
-            return await self.insert(dict)
-
-        id = dict["_id"]
-        dict.pop("_id")
-        await self.db.update_one({"_id": id}, {f"${option}": dict}, *args, **kwargs)
+        data_id = data.pop("_id")
+        await self.db.update_one(
+            {"_id": data_id}, {f"${option}": data}, *args, **kwargs
+        )
 
     async def upsert_custom(
-        self, filter_data, update_data, option="set", *args, **kwargs
+        self, filter_dict, update_data, option="set", *args, **kwargs
     ):
         await self.update_by_custom(
-            filter_data, update_data, option, upsert=True, *args, **kwargs
+            filter_dict, update_data, option, upsert=True, *args, **kwargs
         )
 
     async def update_by_custom(
-        self, filter_data, update_data, option="set", *args, **kwargs
+        self, filter_dict, update_data, option="set", *args, **kwargs
     ):
-        """Update the db with a custom filter, not just id"""
-        if not isinstance(filter_data, collections.abc.Mapping) or not isinstance(
-            update_data, collections.abc.Mapping
-        ):
-            raise TypeError("Expected Dictionary.")
+        self.__ensure_dict(filter_dict)
+        self.__ensure_dict(update_data)
 
-        if not bool(await self.find_by_custom(filter_data)):
+        if not bool(await self.find_by_custom(filter_dict)):
             # Insert
-            return await self.insert({**filter_data, **update_data})
+            return await self.insert({**filter_dict, **update_data})
 
         # Update
         await self.db.update_one(
-            filter_data, {f"${option}": update_data}, *args, **kwargs
+            filter_dict, {f"${option}": update_data}, *args, **kwargs
         )
 
-    async def unset(self, dict):
-        """
-        For when you want to remove a field from
-        a pre-existing document in the collection
-        This function parses an input Dictionary to get
-        the relevant information needed to unset.
-        Params:
-         - dict (Dictionary) : Dictionary to parse for info
-        """
-        # Check if its actually a Dictionary
-        if not isinstance(dict, collections.abc.Mapping):
-            raise TypeError("Expected Dictionary.")
+    async def unset(self, data):
+        self.__ensure_dict(data)
+        self.__ensure_id(data)
 
-        # Always use your own _id
-        if not dict["_id"]:
-            raise KeyError("_id not found in supplied dict.")
-
-        if await self.find_by_id(dict["_id"]) is None:
+        if await self.find_by_id(data["_id"]) is None:
             return
 
-        id = dict["_id"]
-        dict.pop("_id")
-        await self.db.update_one({"_id": id}, {"$unset": dict})
+        data_id = data.pop("_id")
+        await self.db.update_one({"_id": data_id}, {"$unset": data})
 
-    async def increment(self, id, amount, field):
-        """
-        Increment a given `field` by `amount`
-        Params:
-        - id () : The id to search for
-        - amount (int) : Amount to increment by
-        - field () : field to increment
-        """
+    async def increment(self, data_id, amount, field):
         if await self.find_by_id("_id") is None:
             return
 
-        self.db.update_one({"_id": id}, {"$inc": {field: amount}})
+        self.db.update_one({"_id": data_id}, {"$inc": {field: amount}})
 
     # <-- Private methods -->
-    async def __get_raw(self, id):
-        """
-        An internal private method used to eval certain checks
-        within other methods which require the actual data
-        """
-        return await self.db.find_one({"_id": id})
+    async def __get_raw(self, data_id):
+        return await self.db.find_one({"_id": data_id})
+
+    @staticmethod
+    def __ensure_dict(data):
+        if not isinstance(data, collections.abc.Mapping):
+            raise TypeError(f"Expected Dictionary, given {data.__class__}")
+
+    @staticmethod
+    def __ensure_id(data):
+        if not data["_id"]:
+            raise KeyError("_id not found in supplied dict")
