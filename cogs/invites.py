c3cd989ba9742800a371ba31363a6fe6972d5f6f
Skelmis <47520067+Skelmis@users.noreply.github.com>
2021-07-11 02:39:58 +1200

    .

--- cogs/invites.py
+++ cogs/invites.py
@@ -12,6 +12,7 @@ class Invites(commands.Cog):
 
     @commands.Cog.listener()
     async def on_ready(self):
+        print(f"{self.__class__.__name__} Cog has been loaded\n-----")
         await self.tracker.cache_invites()
 
     @commands.Cog.listener()
@@ -19,12 +20,12 @@ class Invites(commands.Cog):
         await self.tracker.update_invite_cache(invite)
 
     @commands.Cog.listener()
-    async def on_guild_join(self, guild):
-        await self.tracker.update_guild_cache(guild)
-
-    @commands.Cog.listener()
     async def on_invite_delete(self, invite):
         await self.tracker.remove_invite_cache(invite)
+
+    @commands.Cog.listener()
+    async def on_guild_join(self, guild):
+        await self.tracker.update_guild_cache(guild)
 
     @commands.Cog.listener()
     async def on_guild_remove(self, guild):
@@ -32,23 +33,32 @@ class Invites(commands.Cog):
 
     @commands.Cog.listener()
     async def on_member_join(self, member):
-        inviter = await self.tracker.fetch_inviter(member)  # inviter is the member who invited
-        data = await self.bot.invites.find(inviter.id)
+        inviter = await self.tracker.fetch_inviter(member)
+        data = await self.bot.invites.find_by_custom(
+            {"guild_id": member.guild.id, "inviter_id": inviter.id}
+        )
         if data is None:
-            data = {"_id": inviter.id, "count": 0, "userInvited": []}
+            data = {
+                "guild_id": member.guild.id,
+                "inviter_id": inviter.id,
+                "count": 0,
+                "invited_users": []
+            }
 
         data["count"] += 1
-        data["usersInvited"].append(member.id)
-        await self.bot.invites.upsert(data)
+        data["invited_users"].append(member.id)
+        await self.bot.invites.upsert_custom(
+            {"guild_id": member.guild.id, "inviter_id": inviter.id}, data
+        )
 
         channel = discord.utils.get(member.guild.text_channels, name="recording")
         embed = discord.Embed(
-            title=f"Welcome {member.display_name}",
+            title=f"Welcome {member.display_name}!",
             description=f"Invited by: {inviter.mention}\nInvites: {data['count']}",
-            timestamp=member.joined_at
+            timestamp=member.joined_at,
         )
         embed.set_thumbnail(url=member.avatar_url)
-        embed.set_footer(text=member.guild.name, icon_url=member.guild.icon_url)
+        embed.set_footer(text=member.guild, icon_url=member.guild.icon_url)
         await channel.send(embed=embed)
 
 
